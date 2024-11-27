2a970cd3f6eb006852142fa9a1120c47d33c5e6d
sevarostov <seva_rostov@mail.ru>
2024-11-27 15:52:04 +0300

    global RateLimiter

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        RateLimiter::for('global', function (Request $request) {
+            return Limit::perMinute(10)->response(function (Request $request, array $headers) {
+                return response('Too Many Requests', 429, $headers);
+            });
+        });
     }
 }
